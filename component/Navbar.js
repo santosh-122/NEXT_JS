630187d24b7c5187e2381390d8de1e357b53039a
santosh-122 <santoshkumark122@gmail.com>
2022-09-11 21:19:06 +0530

    first commit

---
+++ component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>
+                <a href='/'>Home</a>
+            </li>
+            <li><a href='/contact'>contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Navbar from '../component/Navbar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+   <div>
+    <h1>santhu</h1>
+   </div>
+  )
+}
